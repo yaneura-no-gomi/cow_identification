a559db355d58821ffa3cda56dd1f6331c907317e
yaneura-no-gomi <telescope0314@gmail.com>
2020-02-20 23:29:26 +0900

    implementing dataset class is completed

--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,14 @@
+import glob
 import os
 import os.path as osp
-import glob
+import random
 
-import torch
-from torchvision import transforms as T
 import matplotlib.pyplot as plt
-from PIL import Image
+import torch
 import torch.utils.data as data
+from PIL import Image
+from torchvision import transforms as T
+
 
 class ImageTransform():
     
@@ -32,10 +34,12 @@ def visualize_img(path, resize):
     plt.imshow(img_transformed)
     plt.savefig("./result/tmp_transformed.jpg")
 
-def make_datapath_dict(phase='train'):
+def make_datapath_list(phase='train'):
     """
-    return dict : dict[label] -> paths of the cow which has the label
+    return list : [{label:[paths]},{label:[paths]},...]
     """
+
+    datapath_list = []
 
     if phase == 'test':
         root = "/home/Share/cow/data/hogehoge" # need to modify
@@ -47,37 +51,73 @@ def make_datapath_dict(phase='train'):
     dir_list = sorted(glob.glob(dir_list))
     labels = [osp.basename(p) for p in dir_list]
 
-    datapath_dict = dict()
 
     for label in labels:
+        datapath_dict = dict()
         target = osp.join(root, label, "*")
         target_paths = sorted(glob.glob(target))
         datapath_dict[label] = target_paths
+        datapath_list.append(datapath_dict)
 
-    return datapath_dict
+    return datapath_list
 
 class TripletDataset(data.Dataset):
 
     def __init__(self, transform=None, phase='train'):
         self.transform = transform
         self.phase = phase
-        self.fdict = make_datapath_dict(self.phase)
+        self.flist = make_datapath_list(self.phase)
 
     def __len__(self):
         c = 0
         
-        for _, i in self.dict.items():
-            c += len(i)
+        for cow_dict in self.flist:
+            c += len(cow_dict.values) # number of paths per indivisual
         
         return c
 
     def __getitem__(self, idx):
         """
-        At first, pick up the anchor, and positive randomly from same directory.
-        Negative image is randomly chosen from the other directory images.
+        At first, pick up the anchor, and positive randomly from same path list.
+        Negative image is randomly chosen from the other path list.
         """
+
+        anchor_dict = self.flist[idx]
+        for label, path_list in anchor_dict.items():
+            anchor_label = label
+            anchor_paths = path_list
+
+        anchor = random.choice(anchor_paths)
+
+        pos_label = anchor_label
+        pos = anchor
+        while pos != anchor:
+            pos = random.choice(anchor_paths)
+        
+
+        neg_label = anchor_label
+        while neg_label == anchor_label:
+            neg_dict = random.choice(self.flist)
+    
+            for key, path_list in neg_dict.items():
+                neg_label = key
+                neg_paths = path_list
+        neg = random.choice(neg_paths)
+
+        labels = [anchor_label, pos_label, neg_label]
+
+        img_transformed = []
+        for img_p in [anchor, pos, neg]:
+            img = Image.open(img_p)
+            img = self.transform(img)
+            img_transformed.append(img)
+
+        return img_transformed, labels
 
         
 
 if __name__ == "__main__":
-    
+    # flist = make_datapath_list()
+    dataset = TripletDataset(ImageTransform(resize = 100))
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=False)
+    print(dataset.__getitem__(1))
